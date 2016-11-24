839a5098c4c8adb2e3f2737577f08c6bcf1757d5
Raphael Parree <rparree@edc4it.com>
2016-11-24 17:04:39 +0100

    configured for karma/jasmine (unit tests)

---
+++ config/karma-entry.js
@@ -0,0 +1,31 @@
+
+require('core-js/es6');
+require('core-js/es7/reflect');
+
+require('zone.js/dist/zone');
+require('zone.js/dist/long-stack-trace-zone');
+require('zone.js/dist/proxy');
+require('zone.js/dist/sync-test');
+require('zone.js/dist/jasmine-patch');
+require('zone.js/dist/async-test');
+require('zone.js/dist/fake-async-test');
+
+// RxJS
+require('rxjs/Rx');
+const browserTesting = require('@angular/platform-browser-dynamic/testing');
+var testing = require('@angular/core/testing');
+
+testing.TestBed.initTestEnvironment(
+    browserTesting.BrowserDynamicTestingModule,
+    browserTesting.platformBrowserDynamicTesting()
+);
+
+const context = require.context('../src/', true, /\.spec\.ts$/);
+
+
+/*
+ * get all the files, for each file, call the context function
+ * that will require the file and load it up here. Context will
+ * loop and require those spec files here
+ */
+const modules = context.keys().map(context);
